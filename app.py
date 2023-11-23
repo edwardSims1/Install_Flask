df4ae355de74ce5e50df70c79cd872d2f11b2780
Edward Sims Jr <det-10@TheHiddProject4.hsd1.mi.comcast.net>
2023-11-22 20:09:52 -0500

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
+from markupsafe import escape
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index/')
 def genius():
-   return '<h1>Hello,Genius!</h1>'
+        return '<h1>Hello,Genius!</h1>'
+
+@app.route('/about/')
+def about():
+        return '<h3> WOWW!!!If Feels Great To Be A Genius Today! </h3>'
+
+
+@app.route('/capitalize/<word>/')
+def capitalize(word) :
+        return '<h1>{}</h1>'.format(escape(word.capitalize()))
+
+
+@app.route('/add/<int:n1>/<int:n2>/')
+def add(n1,n2):
+        return '<h1>{}</h1>'.format(n1+n2)
+
+@app.route('/users/<int:user_id>/')
+def greet_user (user_id):
+        users = ['Bob', 'Jane', 'Adam'] 
+        try:  
+                return '<h2> Hi {}</h2>' . format(users[user_id])   
+        
+        except IndexError:
+                abort(404)
+
+@app.route('/myName/<first>/<middle>/<last>/')
+def myName(first,middle,last):
+        return '<h1>{}</h1>'.format(first +" "+ middle + " "+ last)
